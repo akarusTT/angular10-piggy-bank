a856455edeb3ba69678fcbf14cc46aa072b0b2bd
akarusTT <rodney.seligman@gmail.com>
2020-08-31 13:07:12 -0400

    Set up repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+import { AccountService } from './_services';
+import { User } from './_models';
+
+@Component({ selector: 'app-root', templateUrl: 'app.component.html' })
 export class AppComponent {
-  title = 'angular10-piggy-bank';
+  user: User;
+
+  constructor(private accountService: AccountService) {
+    this.accountService.user.subscribe((x) => (this.user = x));
+  }
+
+  logout(): void {
+    this.accountService.logout();
+  }
 }
